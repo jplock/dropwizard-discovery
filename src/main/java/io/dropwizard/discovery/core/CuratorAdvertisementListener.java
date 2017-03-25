2fda8543ded6da1af7efbf3b56ceb69d5e40cd77
Justin Plock <jplock@users.noreply.github.com>
2017-03-24 21:28:36 -0400

    Upgrade to Dropwizard 1.1.0-rc1 (#21)
    * Upgrade to Dropwizard 1.1.0-rc1
    
    * Upgrade to Dropwizard v1.1.0-rc2
    
    * Upgrade to Dropwizard v1.1.0-rc3
    
    * Upgrade to DW 1.1.0

--- src/main/java/io/dropwizard/discovery/core/CuratorAdvertisementListener.java
+++ src/main/java/io/dropwizard/discovery/core/CuratorAdvertisementListener.java
@@ -1,21 +1,17 @@
 package io.dropwizard.discovery.core;
 
 import static com.google.common.base.Preconditions.checkNotNull;
-import io.dropwizard.lifecycle.ServerLifecycleListener;
-import java.net.InetSocketAddress;
-import java.nio.channels.ServerSocketChannel;
 import javax.annotation.Nonnull;
-import org.eclipse.jetty.server.Connector;
 import org.eclipse.jetty.server.Server;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import io.dropwizard.lifecycle.ServerLifecycleListener;
 
-public class CuratorAdvertisementListener<T> implements ServerLifecycleListener {
+public class CuratorAdvertisementListener<T>
+        implements ServerLifecycleListener {
 
     private static final Logger LOGGER = LoggerFactory
             .getLogger(CuratorAdvertisementListener.class);
-    private static final String APPLICATION_CONNECTOR = "application";
-    private static final String ADMIN_CONNECTOR = "admin";
     private final CuratorAdvertiser<T> advertiser;
 
     /**
@@ -31,31 +27,8 @@ public class CuratorAdvertisementListener<T> implements ServerLifecycleListener 
 
     @Override
     public void serverStarted(final Server server) {
-        // Detect the port Jetty is listening on - works with configured and
-        // random ports
-        int listenPort = 0;
-        Integer adminPort = null;
-        for (final Connector connector : server.getConnectors()) {
-            try {
-                final ServerSocketChannel channel = (ServerSocketChannel) connector
-                        .getTransport();
-                final InetSocketAddress socket = (InetSocketAddress) channel
-                        .getLocalAddress();
-
-                switch (connector.getName()) {
-                case APPLICATION_CONNECTOR:
-                    listenPort = socket.getPort();
-                    break;
-                case ADMIN_CONNECTOR:
-                    adminPort = socket.getPort();
-                    break;
-                }
-            } catch (Exception e) {
-                LOGGER.error(
-                        "Unable to get port from connector: "
-                                + connector.getName(), e);
-            }
-        }
+        final int listenPort = getLocalPort(server);
+        final int adminPort = getAdminPort(server);
 
         try {
             advertiser.initListenInfo(listenPort, adminPort);
